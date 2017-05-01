c19f25ffd0dc2f1199907911401ea94d59e4ed63
Marc Nijdam <marc@helium.com>
2017-05-01 11:40:40 -0700

    remove host build specifics

--- carbon_interface_host.h
+++
@@ -1,8 +0,0 @@
-#ifndef AM_INTERFACE_HOST_H
-#define AM_INTERFACE_HOST_H
-
-#include "carbon_interface.h"
-
-extern struct carbon_transport_iface const carbon_transport_iface_host;
-
-#endif /* AM_INTERFACE_HOST_H */
