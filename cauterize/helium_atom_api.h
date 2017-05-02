7f920937081ae11cb8b621f7d69d4d7cecc8c45b
Jay Kickliter <jay@kickliter.com>
2017-05-02 11:10:15 -0700

    cauterize: remove `caut-c11-stream` generated code

--- cauterize/helium_atom_api.h
+++
@@ -1,7 +0,0 @@
-#ifndef _CAUTERIZE_C11STREAM_helium_atom_api_
-#define _CAUTERIZE_C11STREAM_helium_atom_api_
-
-#include "helium_atom_api_types.h"
-#include "helium_atom_api_descriptors.h"
-
-#endif /* _CAUTERIZE_C11STREAM_helium_atom_api_ */
