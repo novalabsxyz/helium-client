d4579d7a1217b158c72b10bffab021df08a825e8
Robert Keith <robertfkeith@me.com>
2018-03-25 16:29:21 -0700

    started crappy fragmenting

---
+++ packtize.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <helium-client.h>
+
+void 
+send_message(struct helium_channel * channel,
+            const char *            data,
+            size_t                  m_size,
+            uint16_t *              token,
+            uint8_t                 position)
+{
+    float seg_count = (m_size-position)/HELIUM_MAX_DATA_SIZE;
+    
+    if(seg_count >= 0 && position < m_size){
+        char segment[100];
+        for (int i = 0; i<HELIUM_MAX_DATA_SIZE; i++)
+        {
+            if(position < m_size){
+                segment[i] = data[position];
+                position++;
+            }
+        }
+        int status = helium_channel_send(channel, segment, HELIUM_MAX_DATA_SIZE, token);
+        if(helium_status_OK == status){
+            send_message(channel, data, m_size, token, position);
+        }
+        else{
+            send_message(channel, data, m_size, token, 0);
+        }
+    }
+    else{
+        printf("Done sending"); 
+    }
+
+}
+
